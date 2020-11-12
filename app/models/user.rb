afa9129f83ed5f52e789434d689a2e0804683201
roro1053 <c1053rroro@gmail.com>
2020-11-12 18:38:29 +0900

    passwordが英数混合でないと登録できなように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-validates :password, format: { with: /\A[a-z\d]+\z/}
+validates :password, format: { with:  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze}
 
  with_options presence: true do
 
