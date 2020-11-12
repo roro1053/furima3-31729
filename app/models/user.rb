f23a56ed998f787fab1f37b20c24fde76b795157
roro1053 <c1053rroro@gmail.com>
2020-11-12 17:26:44 +0900

    「Nickname can't be blank」が重複して出ないように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+validates :password, format: { with: /\A[a-z\d]+\z/}
+
  with_options presence: true do
 
   validates :nickname
   validates :birth_date
-  validates :password, format: { with: /\A[a-z\d]+\z/}
+  
 
   with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
     validates :first_name
