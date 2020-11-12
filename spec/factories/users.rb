ab9838a0ee5557aef88bb2ec225747ca93d3d2be
roro1053 <c1053rroro@gmail.com>
2020-11-13 08:32:23 +0900

    fakerとgimeiを導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"00000a"}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password              {password}
     password_confirmation {password}
     first_name            {"苗字"}
     last_name             {"名前"}
