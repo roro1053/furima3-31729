8f1e431a4b4026d4072aa51552f708f40d9c523d
roro1053 <c1053rroro@gmail.com>
2020-11-12 12:12:12 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,presence: true
+  validates :first_name,presence: true
+  validates :last_name,presence: true
+  validates :first_yomigana,presence: true
+  validates :last_yomigana,presence: true
+  validates :birth_date,presence: true
+  
 end
