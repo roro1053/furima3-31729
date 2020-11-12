6143f88cacf4e36fe8fd3f861d17db892bc242a6
roro1053 <c1053rroro@gmail.com>
2020-11-12 14:59:04 +0900

    新規登録フォームに正規表現のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,presence: true
-  validates :first_name,presence: true
-  validates :last_name,presence: true
-  validates :first_yomigana,presence: true
-  validates :last_yomigana,presence: true
-  validates :birth_date,presence: true
-  
+ with_options presence: true do
+
+  validates :nickname
+  validates :birth_date
+  validates :password, format: { with: /\A[a-z\d]+\z/}
+
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options format: { with: /\A[ァ-ン]+\z/} do
+    validates :first_yomigana
+    validates :last_yomigana
+  end
+
+ end
 end
