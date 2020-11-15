bcc32aed0e831946999228dbf4694ca4ce8343e1
roro1053 <c1053rroro@gmail.com>
2020-11-15 11:54:55 +0900

    「発送までの日数を実装」のプルダウンを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,9 +8,11 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :delivery_fee
   belongs_to :shipment_source
+  belongs_to :shipping_day
 
   validates :category_id, numericality: { other_than: 1 } 
   validates :status_id, numericality: { other_than: 1 } 
   validates :delivery_fee_id, numericality: { other_than: 1 } 
   validates :shipment_source_id, numericality: { other_than: 1 } 
+  validates :shipping_day_id, numericality: { other_than: 1 } 
 end
