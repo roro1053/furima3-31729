a7731da0b6a99a93a89536c0211fd1c6206f93f8
roro1053 <c1053rroro@gmail.com>
2020-11-15 10:37:33 +0900

    「配送料の負担」のプルダウンを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :delivery_fee
 
   validates :category_id, numericality: { other_than: 1 } 
   validates :status_id, numericality: { other_than: 1 } 
+  validates :delivery_fee_id, numericality: { other_than: 1 } 
 end
