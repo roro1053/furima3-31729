0621d3ba257ffe623f4aaacd89bc6af887beb2ca
roro1053 <c1053rroro@gmail.com>
2020-11-18 10:37:28 +0900

    編集をするとエラーが出る問題を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,7 +30,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to item_path(item.id), method: :get
+      redirect_to item_path(@item.id), method: :get
     else
       render :edit
     end
