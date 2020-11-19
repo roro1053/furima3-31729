cb8c5700fb3ec1a97a8eb3488738a53753b9c0c1
roro1053 <c1053rroro@gmail.com>
2020-11-19 18:21:02 +0900

    buy_recordのモデルとカラムを設定

--- db/migrate/20201119091816_create_buy_records.rb
+++ db/migrate/20201119091816_create_buy_records.rb
@@ -1,7 +1,8 @@
 class CreateBuyRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :buy_records do |t|
-
+      t.references :user, foreign_key: ture
+      t.references :item, foreign_key: ture
       t.timestamps
     end
   end
