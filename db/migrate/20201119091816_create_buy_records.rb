b60bc3f1349775c24ad54a1dfbbf8b5236017d7e
roro1053 <c1053rroro@gmail.com>
2020-11-19 18:22:00 +0900

    スペルミスを修正

--- db/migrate/20201119091816_create_buy_records.rb
+++ db/migrate/20201119091816_create_buy_records.rb
@@ -1,8 +1,8 @@
 class CreateBuyRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :buy_records do |t|
-      t.references :user, foreign_key: ture
-      t.references :item, foreign_key: ture
+      t.references :user, foreign_key: true
+      t.references :item, foreign_key: true
       t.timestamps
     end
   end
