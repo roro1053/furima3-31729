e2dfdf721fce856df943b0ba40753ac72434f5d5
roro1053 <c1053rroro@gmail.com>
2020-11-19 18:46:01 +0900

    shipping_addressテーブルとカラムを記述

--- db/migrate/20201119093109_create_shipping_addresses.rb
+++ db/migrate/20201119093109_create_shipping_addresses.rb
@@ -1,7 +1,13 @@
 class CreateShippingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :shipping_addresses do |t|
-
+      t.integer :prefecture_id
+      t.string :city
+      t.string :house_number
+      t.string :building_name
+      t.string :phone_number
+      t.string :postal_cord
+      t.references :buy_record
       t.timestamps
     end
   end
