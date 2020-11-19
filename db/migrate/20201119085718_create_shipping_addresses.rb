0536b0f15deeeaa84855f0ddc49108dca916840f
roro1053 <c1053rroro@gmail.com>
2020-11-19 17:59:56 +0900

    shippingモデルのアソシエーションを設定

--- db/migrate/20201119085718_create_shipping_addresses.rb
+++ db/migrate/20201119085718_create_shipping_addresses.rb
@@ -1,8 +1,14 @@
 class CreateShippingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :shipping_addresses do |t|
-
-      t.timestamps
+     # t.integer :prefecture_id 
+     # t.string :city
+     # t.string :house_number
+     # t.string :building_name
+     # t.string :phone_number
+     # t.string :postal_cord
+     # t.references :buy_record, foreign_key: true
+     # t.timestamps
     end
   end
 end
