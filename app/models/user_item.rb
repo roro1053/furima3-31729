15f29748a4e66f712a0db782d5717f9f5b3e71af
roro1053 <c1053rroro@gmail.com>
2020-11-19 18:58:20 +0900

    保存したい複数のテーブルのカラムを記述

--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -1,4 +1,4 @@
-#class UserItem
- # include ActiveModel::Model
-  #attr_accessor :
-#end
+class UserItem
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :prefecture_id, :citr, :house_number, :building_name, :phone_number, :postal_cord, :buy_record
+end
