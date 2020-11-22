2a635dc83921789ee3ff5aa955042fbf2a5e5fbf
roro1053 <c1053rroro@gmail.com>
2020-11-22 10:56:07 +0900

    電話番号を最大11桁しか保存できないようにバリデーションを設定

--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -6,6 +6,7 @@ class UserItem
   validates :city
   validates :house_number
   validates :phone_number, format: {with: /\A[0-9]+\z/}
+  validates :phone_number, length: {maximum: 11}
   validates :postal_cord, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
  end
