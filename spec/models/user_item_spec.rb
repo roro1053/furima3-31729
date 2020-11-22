83cddf16309886306de7746832771cc135bd5232
roro1053 <c1053rroro@gmail.com>
2020-11-22 11:07:24 +0900

    FactoryBotで生成するインスタンスを設定

---
+++ spec/models/user_item_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe UserItem, type: :model do
+  describe "商品の購入" do
+    before do
+      @user_item = FactoryBot.build(:user_item)
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  end
+end
