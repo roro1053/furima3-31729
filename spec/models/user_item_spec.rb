f238e3c9c198839d6209b4ca67369d32743c2bba
roro1053 <c1053rroro@gmail.com>
2020-11-22 11:32:28 +0900

    正常系のテストコードを確認した

--- spec/models/user_item_spec.rb
+++ spec/models/user_item_spec.rb
@@ -8,17 +8,18 @@ RSpec.describe UserItem, type: :model do
 
     context '商品購入がうまくいく時' do
       it "全ての情報が正しく入力された時に購入できる" do
-        binding.pry
         expect(@user_item).to be_valid
       end
 
       it "建物名が空でも購入できる" do
-
+        @user_item.building_name = ""
+        expect(@user_item).to be_valid
       end
-
     end
 
     context '商品出品がうまくいかないとき' do
+      it do
+      end
     end
 
 
