0b5ac0e306f59d7e70197a0fe9cf1dbfdcc0db43
roro1053 <c1053rroro@gmail.com>
2020-11-23 11:28:46 +0900

    売却済の商品に出品者がアクセスするとトップページに遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,9 +27,9 @@ class ItemsController < ApplicationController
     unless user_signed_in? && current_user.id == @item.user_id
     redirect_to action: :index 
     end
-    #unless @item.buy_record.nil?
-     # redirect_to action: :index
-    #end
+    unless @item.buy_record.nil?
+     redirect_to action: :index
+    end
   end
 
   def update
